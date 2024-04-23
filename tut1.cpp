58c2b09b0d71176b695cb88cba8d03802aa06921
rajtilak-patel <rp2380264@gmail.com>
2024-04-23 12:59:57 +0530

    first program

---
+++ tut1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void helloFunction(){
+   cout<<"Hello world"<<endl;
+}
+
+int main() {
+    std::cout << "Hello World! Rajtilak" << endl;
+    helloFunction();
+    std::cout << "Hello World! Patel" << endl;
+    return 0;
+}
+
